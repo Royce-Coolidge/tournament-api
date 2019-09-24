d66e0a49215b7ecc6ec68b6929d1400a661ea796
Rowley thompson <thompsonrjwt.gmail.com>
2019-09-24 15:40:47 +0100

    artisan migrated player table and set player model

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 // when the user makes a POST request to the URL /players
 // call the store method of the Players controller
 $router->post("players", "Players@store");
-$router->get("players", )
+
+$router->get("players", "Players@index"):
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
